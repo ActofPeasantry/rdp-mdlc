8f6b6cd5a2f81512db1c30251e37dde3b401f162
ActofPeasantry <afif.maulana.isman@gmail.com>
2021-11-11 21:50:25 +0700

    preparing for hosting

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://mdlc.herokuapp.com/'),
+    // 'url' => env('APP_URL', 'https://mdlc.herokuapp.com/'),
 
-    'asset_url' => env('ASSET_URL', 'https://mdlc.herokuapp.com/'),
+    // 'asset_url' => env('ASSET_URL', 'https://mdlc.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
