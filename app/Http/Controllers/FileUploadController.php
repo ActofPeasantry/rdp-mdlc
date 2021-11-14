ff14e3f187290ad0505097ebfccde0910d6a4409
ActofPeasantry <afif.maulana.isman@gmail.com>
2021-11-14 21:15:49 +0700

    test public storage

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -10,21 +10,21 @@ class FileUploadController extends Controller
 {
     public function imageUpload(Request $request){
         // dd($request);
-        $file=$request->file('file')->getClientOriginalName();
-        $filename = pathinfo($file, PATHINFO_FILENAME);
-        $extension = pathinfo($file, PATHINFO_EXTENSION);
-        $new_name = $filename.time().'.'.$extension;
+        // $file=$request->file('file')->getClientOriginalName();
+        // $filename = pathinfo($file, PATHINFO_FILENAME);
+        // $extension = pathinfo($file, PATHINFO_EXTENSION);
+        // $new_name = $filename.time().'.'.$extension;
 
-        $disk = Storage::disk('google');
-        $disk->put($new_name, file_get_contents($request->file('file') ));
-        $url = $disk->url($new_name);
-        return response()->json(['location'=>"$url"]);
+        // $disk = Storage::disk('google');
+        // $disk->put($new_name, file_get_contents($request->file('file') ));
+        // $url = $disk->url($new_name);
+        // return response()->json(['location'=>"$url"]);
 
-        // $fileName=$request->file('file')->getClientOriginalName();
-        // $path=$request->file('file')->storeAs('uploads', $fileName, 'public');
-        // return response()->json(['location'=>"/storage/$path"]);
+        $fileName=$request->file('file')->getClientOriginalName();
+        $path=$request->file('file')->storeAs('uploads', $fileName, 'public');
+        return response()->json(['location'=>"/storage/$path"]);
 
-        // $imgpath = request()->file('file')->store('uploads', 'public');
-        // return response()->json(['location' => "/storage/$imgpath"]);
+        $imgpath = request()->file('file')->store('uploads', 'public');
+        return response()->json(['location' => "/storage/$imgpath"]);
     }
 }
